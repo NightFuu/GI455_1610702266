516dc26f4ca6d284003f726f2775fc7cbd705fe0
DESKTOP-AL5NGRG\NightFuu <chitsanupong.inth@bumail.net>
2021-01-27 16:35:58 +0700

    Home work Week1

---
+++ GI455/Assets/TextMatching/Button.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    
+         public string MyName;
+         public GameObject ShowText;
+         public GameObject Poptext;
+ 
+    public void StoreName()
+    {
+        MyName = ShowText.GetComponent<Text>().text;
+        Poptext.GetComponent<Text>().text =  MyName +  " Google is found ";
+    }
+
+}
